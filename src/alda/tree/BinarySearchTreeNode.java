a5a6f85386dec69b852ac38d71c094f55a9f9fe3
aysterberi <filipoppel@hotmail.com>
2016-02-05 10:50:23 +0100

    Fixed

--- src/alda/tree/BinarySearchTreeNode.java
+++ src/alda/tree/BinarySearchTreeNode.java
@@ -7,7 +7,7 @@ package alda.tree;
 
 /**
  * Denna klass representerar noderna i ett binärt sökträd utan dubletter.
- * <p>
+ *
  * Detta är den enda av de tre klasserna ni ska göra några ändringar i. (Om ni
  * inte vill lägga till fler testfall.) De ändringar som är tillåtna är dock
  * begränsade av följande regler:
@@ -18,240 +18,193 @@ package alda.tree;
  * <li>Ni FÅR lägga till fler metoder, dessa ska då vara privata.
  * </ul>
  *
- * @param <T>
  * @author henrikbe
+ *
+ * @param <T>
  */
 public class BinarySearchTreeNode<T extends Comparable<T>> {
 
-	private T data;
-	private BinarySearchTreeNode<T> left;
-	private BinarySearchTreeNode<T> right;
+    private T data;
+    private BinarySearchTreeNode<T> left;
+    private BinarySearchTreeNode<T> right;
 
-	public BinarySearchTreeNode(T data) {
-		this.data = data;
-	}
+    public BinarySearchTreeNode(T data) {
+        this.data = data;
+    }
 
-	/**
-	 * Lägger till en nod i det binära sökträdet. Om noden redan existerar så
-	 * lämnas trädet oförändrat.
-	 *
-	 * @param data datat för noden som ska läggas till.
-	 * @return true om en ny nod lades till trädet.
-	 */
-	public boolean add(T data) {
+    /**
+     * Lägger till en nod i det binära sökträdet. Om noden redan existerar så
+     * lämnas trädet oförändrat.
+     *
+     * @param data datat för noden som ska läggas till.
+     *
+     * @return true om en ny nod lades till trädet.
+     */
+    public boolean add(T data) {
 
-		if (data.equals(this.data) || data == this.data) {
-			return false;
-		} else if (data.compareTo(this.data) < 0) {
-			if (this.left == null) {
-				left = new BinarySearchTreeNode<>(data);
-				return true;
-			} else {
-				return left.add(data);
-			}
-		} else if (data.compareTo(this.data) > 0) {
-			if (this.right == null) {
-				right = new BinarySearchTreeNode<>(data);
-				return true;
-			} else {
-				return right.add(data);
-			}
-		}
-		return false;
-	}
+        if (data.equals(this.data) || data == this.data) {
+            return false;
+        } else if (data.compareTo(this.data) < 0) {
+            if (this.left == null) {
+                left = new BinarySearchTreeNode<>(data);
+                return true;
+            } else {
+                return left.add(data);
+            }
+        } else if (data.compareTo(this.data) > 0) {
+            if (this.right == null) {
+                right = new BinarySearchTreeNode<>(data);
+                return true;
+            } else {
+                return right.add(data);
+            }
+        }
+        return false;
+    }
 
-	/**
-	 * Privat hjälpmetod som är till nytta vid borttag. Ni behöver inte
-	 * skriva/utnyttja denna metod om ni inte vill.
-	 *
-	 * @return det minsta elementet i det (sub)träd som noden utgör root i.
-	 */
-	private BinarySearchTreeNode<T> findMin() {
-		if (left == null) {
-			return this;
-		} else {
-			return left.findMin();
-		}
-	}
+    /**
+     * Privat hjälpmetod som är till nytta vid borttag. Ni behöver inte
+     * skriva/utnyttja denna metod om ni inte vill.
+     *
+     * @return det minsta elementet i det (sub)träd som noden utgör root i.
+     */
+    private BinarySearchTreeNode<T> findMin() {
+        if (left == null) {
+            return this;
+        } else {
+            return left.findMin();
+        }
+    }
 
-	private boolean isNull(T data) {
-		return (data == null) || data.equals(null);
-	}
+    /**
+     * Tar bort ett element ur trädet. Om elementet inte existerar s lämnas
+     * trädet oförändrat.
+     *
+     * @param data elementet som ska tas bort ur trädet.
+     * @return en referens till nodens subträd efter borttaget.
+     */
+    public BinarySearchTreeNode<T> remove(T data) {
+        return removeSquirrel(this, data);
+    }
 
-	private boolean hasChildren() {
-		return (left != null && right != null);
-	}
+    private BinarySearchTreeNode<T> removeSquirrel(BinarySearchTreeNode<T> node, T data) {
+        if (node == null) {
+            return null;
+        }
+        if (data.equals(node.data)) {
+            if (node.left == null && node.right == null) {
+                return null;
+            } else if (node.left == null) {
+                return node.right;
+            } else if (node.right == null) {
+                return node.left;
+            }
 
-	private boolean dataEquals(T Data) {
-		return (this.data.equals(data) || this.data == data);
-	}
+            BinarySearchTreeNode<T> tmp = node.right.findMin();
+            node.data = tmp.data;
+            node.right = removeSquirrel(node.right, tmp.data);
 
-	private boolean hasLeft() {
-		return left != null;
-	}
+            return node;
 
-	private boolean hasRight() {
-		return right != null;
-	}
+        } else if (data.compareTo(node.data) < 0) {
+            node.left = removeSquirrel(node.left, data);
+            return node;
+        } else {
+            node.right = removeSquirrel(node.right, data);
+            return node;
+        }
+    }
 
-	private void clear() {
-		this.data = null;
-		this.left = null;
-		this.right = null;
-	}
+    /**
+     * Kontrollerar om ett givet element finns i det (sub)träd som noden utgör
+     * root i.
+     *
+     * @param data det sökta elementet.
+     * @return true om det sökta elementet finns i det (sub)träd som noden utgör
+     * root i.
+     */
+    public boolean contains(T data) {
+        if (data.equals(this.data) || data == this.data) {
+            return true;
+        } else if (data.compareTo(this.data) < 0) {
+            if (left != null) {
+                return this.left.contains(data);
+            }
+        } else if (data.compareTo(this.data) > 0) {
+            if (right != null) {
+                return this.right.contains(data);
+            }
+        }
+        return false;
+    }
 
-	private void replaceL(BinarySearchTreeNode<T> node) {
-		this.data = node.data;
-		this.right = node.right;
-		this.left = node.left;
-	}
+    /**
+     * Storleken på det (sub)träd som noden utgör root i.
+     *
+     * @return det totala antalet noder i det (sub)träd som noden utgör root i.
+     */
+    public int size() {
+        if ((left == null) && (right == null)) {
+            return 1;
+        }
+        if ((left != null) && (right != null)) {
+            return 1 + left.size() + right.size();
+        }
+        if (left != null && (right == null)) {
+            return 1 + left.size();
+        } else if (left == null && (right != null)) {
+            return 1 + right.size();
+        }
+        return 1;
+    }
 
-	/**
-	 * Tar bort ett element ur trädet. Om elementet inte existerar s lämnas
-	 * trädet oförändrat.
-	 *
-	 * @param data elementet som ska tas bort ur trädet.
-	 * @return en referens till nodens subträd efter borttaget.
-	 */
-	public BinarySearchTreeNode<T> remove(T data) {
-		//TODO: VERIFY
-		// code adapted after Recursive BST Operations
-		// example by Hojjat Ghaderi at University of Toronto
-		if (data == null) {
-		} else if (data.compareTo(this.data) < 0) {
-			if (hasLeft())
-				left = left.remove(data);
-			else {
-				return this;
-			}
-		} else if (data.compareTo(this.data) > 0) {
-			if (hasRight()) {
-				right = right.remove(data);
-			} else {
-				return this;
-			}
-		} else {
-			if (right == null) {
-				if (left != null) {
-					replaceL(left);
-				}
-				return this;
-			} else {
-				BinarySearchTreeNode<T> successor = right.findMin();
-				this.data = successor.data;
-				right = successor.remove(data);
-			}
-		}
-		return this;
-	}
+    /**
+     * Det högsta djupet i det (sub)träd som noden utgör root i.
+     *
+     * @return djupet.
+     */
+    public int depth() {
+        if (left == null && right == null) {
+            return 0;
+        } else if (left != null && right != null) {
+            return 1 + recursiveDepth();
+        } else if (left == null) {
+            return 1 + right.depth();
+        } else if (right == null) {
+            return 1 + left.depth();
+        }
 
-	/**
-	 * Kontrollerar om ett givet element finns i det (sub)träd som noden utgör
-	 * root i.
-	 *
-	 * @param data det sökta elementet.
-	 * @return true om det sökta elementet finns i det (sub)träd som noden utgör
-	 * root i.
-	 */
-	public boolean contains(T data) {
-		if (data.equals(this.data) || data == this.data) {
-			return true;
-		} else if (data.compareTo(this.data) < 0) {
-			return this.left.contains(data);
-		} else if (data.compareTo(this.data) > 0) {
-			return this.right.contains(data);
-		}
-		return false;
-	}
+        return -1;
+    }
 
-	/**
-	 * Storleken på det (sub)träd som noden utgör root i.
-	 *
-	 * @return det totala antalet noder i det (sub)träd som noden utgör root i.
-	 */
-	public int size() {
-		if ((left == null) && (right == null)) {
-			return 1;
-		}
-		if ((left != null) && (right != null)) {
-			return 1 + left.size() + right.size();
-		}
-		if (left != null && (right == null)) {
-			return 1 + left.size();
-		} else if (left == null && (right != null)) {
-			return 1 + right.size();
-		}
-		return 1;
-	}
+    private int recursiveDepth() {
+        return Math.max(left.depth(), right.depth());
+    }
 
-	/**
-	 * Det högsta djupet i det (sub)träd som noden utgör root i.
-	 *
-	 * @return djupet.
-	 */
-	public int depth() {
-		if (left == null && right == null) {
-			return 0;
-		} else if (left != null && right != null) {
-			return 1 + recursiveDepth();
-		} else if (left == null) {
-			return 1 + right.depth();
-		} else if (right == null) {
-			return 1 + left.depth();
-		}
+    /**
+     * Returnerar en strängrepresentation för det (sub)träd som noden utgör root
+     * i. Denna representation består av elementens dataobjekt i sorterad
+     * ordning med ", " mellan elementen.
+     *
+     * @return strängrepresentationen för det (sub)träd som noden utgör root i.
+     */
+    @Override
+    public String toString() {
+        String str = "";
 
-		return -1;
-	}
+        if (data == null) {
+            return null;
+        }
+        if (left != null) {
+            str += left;
+            str += ", ";
+        }
 
-	private int recursiveDepth() {
-		return Math.max(left.depth(), right.depth());
-	}
-
-	/**
-	 * Returnerar en strängrepresentation för det (sub)träd som noden utgör root
-	 * i. Denna representation består av elementens dataobjekt i sorterad
-	 * ordning med ", " mellan elementen.
-	 *
-	 * @return strängrepresentationen för det (sub)träd som noden utgör root i.
-	 */
-	public String toString() {
-
-		StringBuilder sb = new StringBuilder();
-		if (!hasChildren()) {
-			// we have no children, we're a leaf
-			return data.toString();
-		}
-		toStringSquirrel(this, sb);
-		return sb.toString();
-	}
-
-	/**
-	 * A toString Squirrel function,  O(n)
-	 * Recursively goes through each node in the tree in-order
-	 * (left subtree, node, right subtree) calling node.data
-	 * and appending commas when necessary.
-	 *
-	 * @param node    represents the start node of the tree to be traversed
-	 * @param builder represents the StringBuilder the parent method (toString)
-	 *                is using to build up a returnable String. All operations will be
-	 *                carried out by appending the data to this object.
-	 */
-	private void toStringSquirrel(BinarySearchTreeNode<T> node, StringBuilder builder) {
-		if (node == null) {
-			return;
-		}
-		if (!hasChildren()) {
-			builder.append(node.data.toString());
-		}
-		if (node.left != null) {
-			toStringSquirrel(node.left, builder);
-			builder.append(", ");
-		}
-		builder.append(node.data.toString());
-		if (node.right != null) {
-			builder.append(", ");
-			toStringSquirrel(node.right, builder);
-		}
-
-	}
+        str += data;
+        if (right != null) {
+            str += ", ";
+            str += right;
+        }
+        return str;
+    }
 }
